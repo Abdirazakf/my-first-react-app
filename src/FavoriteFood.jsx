90938665882e159331b0ef439f81c78b78ede473
Abdirazakf <farahabdirazak13@gmail.com>
2025-09-26 00:10:55 -0400

    Finished components lesson

---
+++ src/FavoriteFood.jsx
@@ -0,0 +1,16 @@
+export default function FavoriteFood() {
+    return <div>
+        <h1>My Favorite Foods</h1>
+        <ul>
+            <li>
+                Rice
+            </li>
+            <li>
+                Banana
+            </li>
+            <li>
+                Salmon
+            </li>
+        </ul>
+    </div>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {Greeting} from './Greeting.jsx'
+import FavoriteFood from './FavoriteFood.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FavoriteFood />
   </StrictMode>,
 )
