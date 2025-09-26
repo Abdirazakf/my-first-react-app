fbeade3f13fe96a774a935ba6f6b710794d2318d
Abdirazakf <farahabdirazak13@gmail.com>
2025-09-26 00:31:53 -0400

    Completed jsx lesson

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {Greeting} from './Greeting.jsx'
 import FavoriteFood from './FavoriteFood.jsx'
+import HtmlToJSX from './HtmlToJSX.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FavoriteFood />
+    <HtmlToJSX />
   </StrictMode>,
 )
